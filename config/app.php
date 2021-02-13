50e30d95ed7b86d685269f039953e2da5775fcb4
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-02-13 14:28:27 +0630

    deleted app_local.php and modified DB setting in app.php

--- config/app.php
+++ config/app.php
@@ -229,9 +229,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'root',
+            'password' => 'root',
+            'database' => 'products_ranking_system',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
