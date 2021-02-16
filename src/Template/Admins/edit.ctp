70a866df6ae07612835cc3b67ee81dc870716449
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-02-16 14:33:21 +0630

    Baked Admins

---
+++ src/Template/Admins/edit.ctp
@@ -0,0 +1,33 @@
+<?php
+/**
+  * @var \App\View\AppView $this
+  */
+?>
+<nav class="large-3 medium-4 columns" id="actions-sidebar">
+    <ul class="side-nav">
+        <li class="heading"><?= __('Actions') ?></li>
+        <li><?= $this->Form->postLink(
+                __('Delete'),
+                ['action' => 'delete', $admin->admin_id],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $admin->admin_id)]
+            )
+        ?></li>
+        <li><?= $this->Html->link(__('List Admins'), ['action' => 'index']) ?></li>
+        <li><?= $this->Html->link(__('List Admins'), ['controller' => 'Admins', 'action' => 'index']) ?></li>
+        <li><?= $this->Html->link(__('New Admin'), ['controller' => 'Admins', 'action' => 'add']) ?></li>
+    </ul>
+</nav>
+<div class="admins form large-9 medium-8 columns content">
+    <?= $this->Form->create($admin) ?>
+    <fieldset>
+        <legend><?= __('Edit Admin') ?></legend>
+        <?php
+            echo $this->Form->control('email');
+            echo $this->Form->control('password');
+            echo $this->Form->control('del_flg');
+            echo $this->Form->control('token');
+        ?>
+    </fieldset>
+    <?= $this->Form->button(__('Submit')) ?>
+    <?= $this->Form->end() ?>
+</div>
---
+++ tests/Fixture/AdminsFixture.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * AdminsFixture
+ *
+ */
+class AdminsFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'admin_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'email' => ['type' => 'string', 'length' => 50, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'password' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'del_flg' => ['type' => 'string', 'length' => null, 'null' => false, 'default' => 'not', 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'token' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['admin_id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8mb4_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'admin_id' => 1,
+            'email' => 'Lorem ipsum dolor sit amet',
+            'password' => 'Lorem ipsum dolor sit amet',
+            'del_flg' => 'Lorem ipsum dolor sit amet',
+            'created' => '2021-02-16 08:01:07',
+            'modified' => '2021-02-16 08:01:07',
+            'token' => 'Lorem ipsum dolor sit amet'
+        ],
+    ];
+}
