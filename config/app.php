d7f7d6ad7a865560610092e895148cd9e3634ea4
Htet <cake123php@gmail.com>
2021-03-09 15:12:37 +0630

    luckydraw commit

--- config/app.php
+++ config/app.php
@@ -222,15 +222,15 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => 'localhost:336',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'root',
-            'password' => 'root',
+            'username' => 'admin',
+            'password' => '',
             'database' => 'products_ranking_system',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
